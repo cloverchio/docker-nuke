5ef0eb7e5ec1528a8549e165b9e157a7f87fac5f
cloverchio <chrisloverchio@gmail.com>
2025-06-14 19:03:00 -0600

    added networks object

--- cmd/nuke.go
+++ cmd/nuke.go
@@ -3,38 +3,59 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"github.com/cloverchio/docker-nuke/internal/flags"
+	"github.com/cloverchio/docker-nuke/internal/flag"
+	"github.com/cloverchio/docker-nuke/internal/object"
 	"github.com/cloverchio/docker-nuke/pkg"
 )
 
 func ProcessNuke(subCommands []string) (bool, error) {
-	nuke := flags.NukeFlagSet()
+	nuke := flag.NukeFlagSet()
 	nuke.Usage = pkg.Usage()
 	err := nuke.Parse(subCommands)
 	if err != nil {
 		nuke.Usage()
 		return false, fmt.Errorf("Error parsing flags: %v", err)
 	}
-	if *flags.All {
+	if *flag.All {
 		fmt.Println(pkg.SubCommandMessage("unused", "containers, images, volumes, and networks"))
 		return true, nil
 	}
-	if *flags.Containers {
+	if *flag.Containers {
 		fmt.Println(pkg.SubCommandMessage("stopped", "containers"))
+		containerRemoveError := object.RemoveAllContainers()
+		if containerRemoveError != nil {
+			return false, containerRemoveError
+		}
 	}
-	if *flags.Images {
+	if *flag.Images {
 		fmt.Println(pkg.SubCommandMessage("dangling", "images"))
+		imageRemoveError := object.RemoveDanglingImages()
+		if imageRemoveError != nil {
+			return false, imageRemoveError
+		}
 	}
-	if *flags.AllImages {
+	if *flag.AllImages {
 		fmt.Println(pkg.SubCommandMessage("unused", "images"))
+		imageRemoveError := object.RemoveAllImages()
+		if imageRemoveError != nil {
+			return false, imageRemoveError
+		}
 	}
-	if *flags.Volumes {
+	if *flag.Volumes {
 		fmt.Println(pkg.SubCommandMessage("unused", "volumes"))
+		volumeRemoveError := object.RemoveAllVolumes()
+		if volumeRemoveError != nil {
+			return false, volumeRemoveError
+		}
 	}
-	if *flags.Networks {
+	if *flag.Networks {
 		fmt.Println(pkg.SubCommandMessage("unused", "networks"))
+		networkRemoveError := object.RemoveAllNetworks()
+		if networkRemoveError != nil {
+			return false, networkRemoveError
+		}
 	}
-	if !(*flags.Containers || *flags.Images || *flags.AllImages || *flags.Volumes || *flags.Networks) {
+	if !(*flag.Containers || *flag.Images || *flag.AllImages || *flag.Volumes || *flag.Networks) {
 		nuke.Usage()
 		return false, errors.New("Please specify at least one flag to nuke: --containers, --images, --volumes, --networks, or --all")
 	}
--- internal/object/containers.go
+++ internal/object/containers.go
@@ -1,50 +1,50 @@
 package object
 
 import (
-	"context"
-	"fmt"
-	"github.com/docker/docker/api/types"
-	"github.com/cloverchio/docker-nuke/internal/client"
+    "context"
+    "fmt"
+    "github.com/docker/docker/api/types"
+    "github.com/cloverchio/docker-nuke/internal/client"
 )
 
 func StopAllContainers() error {
-	containers, containerListError := client.Docker.ContainerList(context.Background(), types.ContainerListOptions{All: true})
-	if containerListError != nil {
-		fmt.Printf("Error listing containers: %v\n", containerListError)
-		return containerListError
-	}
-	for _, container := range containers {
-		containerStopError := client.Docker.ContainerStop(context.Background(), container.ID, nil)
-		if containerStopError != nil {
-			fmt.Printf("Error stopping container %s: %v\n", container.ID, containerStopError)
-			return containerStopError
-		}
-		fmt.Printf("Successfully stopped container %s\n", container.ID)
-	}
-	return nil
+    containers, containerListError := client.Docker.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+    if containerListError != nil {
+        fmt.Printf("Error listing containers: %v\n", containerListError)
+        return containerListError
+    }
+    for _, container := range containers {
+        containerStopError := client.Docker.ContainerStop(context.Background(), container.ID, nil)
+        if containerStopError != nil {
+            fmt.Printf("Error stopping container %s: %v\n", container.ID, containerStopError)
+            return containerStopError
+        }
+        fmt.Printf("Successfully stopped container %s\n", container.ID)
+    }
+    return nil
 }
 
 func RemoveAllContainers() error {
-	containerStopError := StopContainers()
-	if containerStopError != nil {
-		return containerStopError
-	}
-	containers, containerListError := client.Docker.ContainerList(context.Background(), types.ContainerListOptions{All: true})
-	if containerListError != nil {
-		containerListErrorMessage(containerListError)
-		return containerListError
-	}
-	for _, container := range containers {
-		containerRemoveError := client.Docker.ContainerRemove(context.Background(), container.ID, types.ContainerRemoveOptions{Force: true})
-		if containerRemoveError != nil {
-			fmt.Printf("Error removing container %s: %v\n", container.ID, containerRemoveError)
-			return containerRemoveError
-		}
-		fmt.Printf("Successfully removed container %s\n", container.ID)
-	}
-	return nil
+    containerStopError := StopAllContainers()
+    if containerStopError != nil {
+        return containerStopError
+    }
+    containers, containerListError := client.Docker.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+    if containerListError != nil {
+        containerListErrorMessage(containerListError)
+        return containerListError
+    }
+    for _, container := range containers {
+        containerRemoveError := client.Docker.ContainerRemove(context.Background(), container.ID, types.ContainerRemoveOptions{Force: true})
+        if containerRemoveError != nil {
+            fmt.Printf("Error removing container %s: %v\n", container.ID, containerRemoveError)
+            return containerRemoveError
+        }
+        fmt.Printf("Successfully removed container %s\n", container.ID)
+    }
+    return nil
 }
 
 func containerListErrorMessage(containerListError error) {
-	fmt.Printf("Error listing containers: %v\n", containerListError)
+    fmt.Printf("Error listing containers: %v\n", containerListError)
 }
